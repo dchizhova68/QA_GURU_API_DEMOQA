45e9d9a81eba6c6486db1d457ad4ead29422a828
d.chizhova <dchizhova68@gmail.com>
2024-02-23 18:25:49 +0400

    add consent cookie

--- src/test/java/tests/CollectionTests.java
+++ src/test/java/tests/CollectionTests.java
@@ -6,8 +6,8 @@ import models.loginModel.LoginResponseModel;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class CollectionTests extends TestBase {
@@ -15,7 +15,7 @@ public class CollectionTests extends TestBase {
     void deleteBookTest() {
 
         LoginResponseModel authResponse = step("Отправляем запрос на авторизацию", () ->
-                AuthorisationApi.authResponse("studentDemoQA11","PassW0rd!")
+                AuthorisationApi.authResponse("studentDemoQA11", "PassW0rd!")
         );
 
         step("Удаляем все книги из Profile", () ->
@@ -26,22 +26,30 @@ public class CollectionTests extends TestBase {
                 BooksApi.addBooks(authResponse.getToken(), authResponse.getUserId())
         );
 
+        step("Принимаем соглашение о куках", () -> {
+            if ($(".fc-consent-root").isDisplayed()) {
+                $(".fc-consent-root").$(byText("Consent")).click();
+            }
+        });
+
         step("Открываем Profile", () ->
                 open("/profile")
         );
+
         step("Проверяем, что книга есть в корзине", () ->
                 $(".ReactTable").shouldHave(text("Understanding ECMAScript 6"))
         );
 
-        step("Открываем корзину", () ->
+
+        step("Нажимаем на иконку корзины", () ->
                 $("#delete-record-undefined").click()
         );
 
-        step("Удаляем книгу", () ->
+        step("Подтверждаем удаление", () ->
                 $("#closeSmallModal-ok").click()
         );
 
-        step("Проверяем, что книги нет в корзине", () ->
+        step("Проверяем, что книги нет в profile", () ->
                 $(".ReactTable").shouldNotHave(text("Understanding ECMAScript 6"))
         );
 
